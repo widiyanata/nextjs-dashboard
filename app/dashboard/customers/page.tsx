25515be93bac01e1aab66f1418d6c410b7aa61df
widiyanata <widyanata48@gmail.com>
2024-01-17 10:58:56 +0700

    + dashboard page

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,7 @@
+export default function Customer() {
+  return (
+    <div>
+      <h1>Customer</h1>
+    </div>
+  );
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,7 @@
+export default function Invoice() {
+  return (
+    <div>
+      <h1>Invoice</h1>
+    </div>
+  );
+}
